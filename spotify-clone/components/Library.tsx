6d2572508bd124c487bc6fd88052cd82a8d30d70
Dragan Subotic <dragan.subi7@gmail.com>
2024-04-01 18:48:53 +0200

    add upload modal basic functionality

--- spotify-clone/components/Library.tsx
+++ spotify-clone/components/Library.tsx
@@ -1,11 +1,22 @@
 "use client"
 
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
+import { useUser } from "@/hooks/useUser";
 import { AiOutlinePlus } from "react-icons/ai";
 import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const { user, subscription } = useUser();
+  const uploadModal = useUploadModal();
   const onClick = () => {
-    // handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: check for subscription
+    return uploadModal.onOpen();
   }
   return (
     <div className="flex flex-col">
