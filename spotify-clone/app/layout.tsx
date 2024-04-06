8d77a25bedd0a324d50bfae79db7f9b8a4c7b52b
Dragan Subotic <dragan.subi7@gmail.com>
2024-04-06 18:48:36 +0200

    add hooks for fetching the songs for player

--- spotify-clone/app/layout.tsx
+++ spotify-clone/app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
 import ToasterProvider from "@/providers/ToastrProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
+import Player from "@/components/Player";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -34,6 +35,7 @@ export default async function RootLayout({
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
+            <Player />
           </UserProvider>
         </SupabaseProvider>
       </body>
