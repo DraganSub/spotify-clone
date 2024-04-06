5c80544f1bca2092096dd1297882ad304cd1421b
Dragan Subotic <dragan.subi7@gmail.com>
2024-04-06 19:12:55 +0200

    add basic look to the player component

--- spotify-clone/components/Player.tsx
+++ spotify-clone/components/Player.tsx
@@ -3,20 +3,21 @@
 import useGetSongById from "@/hooks/useGetSongById";
 import useLoadSong from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
 
   const songUrl = useLoadSong(song!);
-  /* 
-    if (!song || !songUrl || !player.activeId) {
-      return null;
-    } */
+
+  if (!song || !songUrl || !player.activeId) {
+    return null;
+  }
 
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      Player
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   )
 }
