8e07eb50c10f9810e0c31848763f52a331405d8b
Dragan Subotic <dragan.subi7@gmail.com>
2024-04-07 21:50:25 +0200

    add stripe webhooks for premium users

--- spotify-clone/types.ts
+++ spotify-clone/types.ts
@@ -1,4 +1,42 @@
-import Stripe from "stripe";
+import Stripe from 'stripe';
+
+export interface Song {
+  id: string;
+  user_id: string;
+  author: string;
+  title: string;
+  song_path: string;
+  image_path: string;
+}
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
+}
+
+export interface Price {
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  description?: string;
+  unit_amount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metadata?: Stripe.Metadata;
+  products?: Product;
+}
+
+export interface Customer {
+  id: string;
+  stripe_customer_id?: string;
+}
 
 export interface UserDetails {
   id: string;
@@ -8,6 +46,10 @@ export interface UserDetails {
   avatar_url?: string;
   billing_address?: Stripe.Address;
   payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface ProductWithPrice extends Product {
+  prices?: Price[];
 }
 
 export interface Subscription {
@@ -26,38 +68,5 @@ export interface Subscription {
   canceled_at?: string;
   trial_start?: string;
   trial_end?: string;
-  prices?: Price
-}
-
-export interface Price {
-  id: string;
-  product_id?: string;
-  active?: boolean;
-  description?: string;
-  unit_ammount?: number;
-  currency?: string;
-  type?: Stripe.Price.Type;
-  interval?: Stripe.Price.Recurring.Interval;
-  interval_count?: number;
-  trial_period_days?: number | null;
-  metadata?: Stripe.Metadata;
-  products?: Product;
-}
-
-export interface Product {
-  id: string;
-  active?: boolean;
-  name?: string;
-  description?: string;
-  image?: string;
-  metadata?: Stripe.Metadata;
-}
-
-export interface Song {
-  id: string;
-  user_id: string;
-  author: string;
-  title: string;
-  song_path: string;
-  image_path: string;
+  prices?: Price;
 }
